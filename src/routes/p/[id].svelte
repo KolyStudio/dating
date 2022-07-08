dedf79fa62aa40ab79ad18c08b33e73accb568af
Pierre Torres <kolystudio@outlook.com>
2022-07-08 16:16:37 +0200

    first commit

--- src/routes/p/[id].svelte
+++ src/routes/p/[id].svelte
@@ -38,8 +38,6 @@
   <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 </svelte:head>
 
-<!-- <div class="p-1 text-center text-lg font-medium uppercase text-white backdrop-brightness-[.70]">FERMETURE DE LA PAGE Privée à minuit</div> -->
-
 <!-- Logo - Titre -->
 <img src="/heart.svg" alt="heart" class=" fixed top-10 left-5 z-[-1] w-[30px] -rotate-[40deg]" />
 <img src="/heart.svg" alt="heart" class=" fixed top-8 right-5 z-[-1] w-[20px] -rotate-[20deg]" />
