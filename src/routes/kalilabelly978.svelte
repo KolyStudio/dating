3966ebd7f62331d4c06c15e465397b95ff16aef5
Pierre Torres <kolystudio@outlook.com>
2022-07-20 22:13:19 +0200

    first commit

--- src/routes/kalilabelly978.svelte
+++ src/routes/kalilabelly978.svelte
@@ -11,11 +11,11 @@
   const date = dayjs().format('DD/MM/YYYY')
 
   let link =
-    `https://www.rdv-proches.com/?acme=wid.90642&cid=&gemail=&media=social&s1=txt_sm&s2=` +
+    `https://www.rdv-proches.com/?acme=wid.90642&cid=&gemail=&media=social&s1=texte_sm&s2=` +
     date +
-    `&siteid=3461948&source=RENCONTRE&tpl=153&tpls=73&tr4ck=qsgef62d522d20007bb9e&userAge=&userName=` +
+    `&siteid=3461948&tpl=153&tpls=73&tr4ck=qsgef62d522d20007bb9e&userAge=&userName=` +
     name +
-    `25&userDistance=7&userPicture=https%3A%2F%2Fi.ibb.co%2F551c5pr%2Fprofil.webp&v=&xa=n&userPhotos=53`
+    `&userDistance=7&userPicture=https%3A%2F%2Fi.ibb.co%2F551c5pr%2Fprofil.webp&v=&xa=n&userPhotos=53`
 
   let isClicked
 
@@ -29,7 +29,7 @@
     const finalip = await axios.get(`https://api.ipregistry.co/` + `${firstip.data.IPv4}` + `?key=6nn8zr4k2hcwkw32`)
 
     if (!finalip.data.carrier.name) {
-      myip = ``
+      myip = `vers ` + finalip.data.location.city
     } else {
       myip = ``
     }
