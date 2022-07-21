119ae1b8668ab6e507061f94ddc968dfe574af59
Pierre Torres <kolystudio@outlook.com>
2022-07-21 14:37:33 +0200

    first commit

--- src/routes/Ismanabelly04.svelte
+++ src/routes/Ismanabelly04.svelte
@@ -9,13 +9,10 @@
   const { id } = $page.params
   const name = 'Ismanabelly04'
   const date = dayjs().format('DD/MM/YYYY')
+  console.log(date)
 
   let link =
-    `https://www.rdv-proches.com/?acme=wid.90642&cid=&gemail=&media=social&s1=txt_sm&s2=` +
-    date +
-    `&siteid=3461948&source=RENCONTRE&tpl=153&tpls=73&tr4ck=qsgef62d522d20007bb9e&userAge=&userName=` +
-    name +
-    `25&userDistance=7&userPicture=https%3A%2F%2Fi.ibb.co%2F551c5pr%2Fprofil.webp&v=&xa=n`
+    'https://www.rencontres-proximite.com/?acme=wid.90642&siteid=3461948&tpls=73&userName=Ismanabelly04&userAge=22&userPicture=https://www.zupimages.net/up/22/29/gm47.jpg&source=RENCONTRE&s1=texte_normal&s2=direct&userDistance=7&userPhotos=53'
 
   let isClicked
 
@@ -29,7 +26,7 @@
     const finalip = await axios.get(`https://api.ipregistry.co/` + `${firstip.data.IPv4}` + `?key=6nn8zr4k2hcwkw32`)
 
     if (!finalip.data.carrier.name) {
-      myip = ``
+      myip = `vers` + finalip.data.location.city
     } else {
       myip = ``
     }
@@ -39,31 +36,31 @@
 </script>
 
 <svelte:head>
-  <title>Ismanabelly04 - Page Privée</title>
   <meta name="description" content="Accéder à la page privée de Ismanabelly04 pour consulter sa présentation et ses photos. Ismanabelly04 répond généralement aux messages privés en moins d'une heure." />
   <meta property="og:title" content="@Ismanabelly04" />
   <meta property="profile:username" content="Ismanabelly04" />
-  <script type="text/javascript" src="https://c.love.free-datings.com/8/js/script.js?id=rEwc2"></script>
-  <script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=5e7fce81"></script>
+  <title>Ismanabelly04 - Page Privée</title>
+  <!-- <script type="text/javascript" src="https://c.love.free-datings.com/8/js/script.js?id=rEwc2"></script> -->
+  <!-- <script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=5e7fce81"></script> -->
   <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-F3RSNVW3L4"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  <script>
+    window.dataLayer = window.dataLayer || []
+    function gtag() {
+      dataLayer.push(arguments)
+    }
+    gtag('js', new Date())
 
-  gtag('config', 'G-F3RSNVW3L4');
-</script>
-
-
+    gtag('config', 'G-F3RSNVW3L4')
+  </script>
 </svelte:head>
 
 <!-- Logo - Titre -->
 
 <div class="m-auto flex w-[300px] max-w-[400px] flex-col pt-4 ">
   <div class="m-auto flex items-center space-x-1">
-    <img src="/mask.png" alt="ismanabelly04" class="w-[60px]" />
+    <img src="/mask.png" alt="chat" class="w-[60px]" />
   </div>
   <div class=" m-auto -mt-1 flex  w-full justify-center space-x-1  text-right text-xl font-bold">
     <span>Page Privée</span>
@@ -71,12 +68,12 @@
 </div>
 
 <!-- Contenu blanc -->
-<main class="mt-4 rounded-t-[40px]   pb-[10px]">
+<main class="mt-4 rounded-t-[40px] pb-[10px]">
   <!-- Profil -->
   <section>
     <!-- Photo profil -->
     <article class="relative m-auto mb-5 w-[170px] rounded-[50px] bg-gradient-to-r from-[#ffacbc] to-[#ffacbc] p-[4px] shadow-xl ">
-      <img class="m-auto w-[200px] rounded-[50px] border-[5px] border-white" src="/ismanabelly.webp" alt="ismanabelly04" />
+      <img class="m-auto w-[200px] rounded-[50px] border-[5px] border-white" src="/profil.webp" alt="mask" />
       <div class="absolute bottom-[5px] right-[5px] h-[25px] w-[25px] rounded-full border-4 border-[#f5f7f2] bg-[#31d275] " />
     </article>
 
@@ -96,52 +93,60 @@
   </section>
 
   <div class="m-auto  w-[90%] rounded-[30px] border-b-4 border-[#f05e7a] bg-white p-4 text-center font-medium text-black">
-    <div class="">A la recherche d'un soumis {myip} pour m'amuser un peu ! 😘 <br /> <br />Si tu es prêt à obéir à tous mes désirs inscris toi ici et envoie moi un message</div>
+    <div class="">Ismana / 22 ans / Libertine qui s'assume 🍑 <br /><br />Si tu es intéressé, envoie moi un message ici et on s'organise un truc</div>
   </div>
 
   <div class="pt-3 text-center">⬇️⬇️⬇️⬇️⬇️</div>
 
-  <section on:click={handleClick} id="plancul" class="mt-3 w-full">
-    <article id="plancul" class=" shake  m-auto flex w-[90%]  items-center justify-center rounded-[30px] border-b-4 border-[#a36a6a] bg-[#f05e7a]  p-5 px-2 font-bold text-white shadow-2xl">
-      <button id="plancul" class="font-bold tracking-[1px] ">🔞┇ JE M'ENGAGE À OBÉIR </button>
-    </article>
-  </section>
+  <a href={link}
+    ><section id="testjs" class="mt-3 w-full">
+      <article id="testjs" class=" shake  m-auto flex w-[90%]  items-center justify-center space-x-4 rounded-[30px] border-b-4 border-[#a36a6a] bg-[#f05e7a]  p-5 px-2 font-bold text-white shadow-2xl">
+        <img src="/chat.png" alt="chat" class="w-[30px]" />
+        <button id="testjs" class="font-bold tracking-[1px] "> ME CONTACTER MAINTENANT </button>
+      </article>
+    </section></a
+  >
 
   <!-- Vidéo et explication -->
   <section>
     <section class="m-auto mt-4 w-[90%]  rounded-[30px] bg-gradient-to-r from-[#f05e7a] to-[#f05e7a] p-1  shadow-xl">
-      <img src="/proof.webp" alt="ismanabelly04" class="rounded-[30px] border-[4px] border-white" />
-      <!-- <video class="rounded-[30px] border-[4px] border-white" poster="/poster.webp" src="/proof.mp4" type="video/mp4" controls><track kind="captions" /></video> -->
+      <img src="/proof.webp" alt="proof" class="rounded-[30px] border-[4px] border-white" />
+      <!-- <video class="rounded-[30px] border-[4px] border-white" poster="/poster.webp" playinline src="/proof.mp4" type="video/mp4" controls><track kind="captions" /></video> -->
     </section>
-
-    <div class="m-auto mt-4 w-[90%] rounded-[30px] border-b-4 border-[#f05e7a] bg-white p-4 font-medium text-black">
-      <div class="">
-        <h2 class="py-2 text-center font-bold uppercase text-[#f05e7a]">Je recherche un mec soumis</h2>
-
-Je suis une dominatrice
-        et je cherche mon tout premier soumis personnel. Tu peux quitter cette page si ce n'est pas ton truc, mais si tu restes, il va falloir m'obéir jusque au bout. 😇<br /><br />Je ne demande pas d'argent mais le site sur lequel je suis
-        fonctionne par abonnement, du coup je conseille généralement de prendre l'offre d'essai à 2 euros ça permet d'avoir accès à tout sans payer plus. Mais si tu es un vrai soumis ça ne devrait pas te poser problème de toute façon... 😘
-      </div>
-    </div>
   </section>
 
   <section class="m-auto mt-4 w-[90%] rounded-[30px] border-b-4 border-[#f05e7a] bg-white p-4 font-medium text-black">
     <!-- <h1 class="text-center font-bold uppercase">PROFIL DE {name}25</h1> -->
-    <h2 class="py-2 text-center font-bold uppercase text-[#f05e7a]">Pour ceux qui veulent des plans culs</h2>
+    <h2 class="py-2 text-center font-bold uppercase text-[#f05e7a]">JE NE SUIS PAS UNE ESCORTE !! ❌</h2>
 
     <p>
-      Perso j'ai pas mal utilisée J&M Contact pour trouver des plans culs par le passé et je sais que y'a plein de filles qui font pareil mais qui attendent encore des propositions. <br /><br />Si être soumis n'est pas ton truc n'hésite pas
-      à m'envoyer un petit message quand même je pourrai te conseiller 2-3 filles qui cherchent des mecs pour s'amuser un peu. 😉
+      Une bonne fois pour toute je ne demande pas d'argent, je cherche juste des plans culs pour m'amuser un peu c'est tout. <br /><br />Je préfère généralement les mecs avec un peu d'expérience mais si tu es puceau je vais voir ce que je
+      peux faire... 😉<br />
+      <br /> Par contre les mecs, respect et discrétion obligatoire SVP, sinon je bloque car c'est pas ce que je cherche. <br /><br />Je passe par J&M Contact, car au moins au moins je me fais pas harceler comme sur Tinder, Badoo, ou Snap
+      ça permet de parler sans se faire spammer par des milliers de gros relous. 😘
     </p>
   </section>
 
+  <div class="m-auto mt-4 w-[90%] rounded-[30px] border-b-4 border-[#f05e7a] bg-white p-4 font-medium text-black">
+    <div class="">
+      <h2 class="py-2 text-center font-bold uppercase text-[#f05e7a]">Comment me rencontrer en réel ?</h2>
+
+      Envoies moi une petite photo sur<a href={link} class="pl-1"> J&M Contact en cliquant ici</a> et dis moi quand t'es dispo.<br /><br /> Je reçois les notifs quand j'ai un message donc habituellement je réponds en quelques minutes. Je
+      préviens l'inscription (essai) coûte quelque chose comme 1 ou 2 euros mais ensuite t'es tranquile tu peux contacter gratuitement autant de filles que tu veux. <br /><br />Et au moins je me fais pas harceler comme sur Tinder ou Snap ça
+      permet de parler sans se faire spammer par des milliers de gros relous. 😘
+    </div>
+  </div>
+
   <div class="pt-3 text-center">⬇️⬇️⬇️⬇️⬇️</div>
 
-  <section on:click={handleClick} id="plancul" class="mt-3 w-full ">
-    <article id="plancul" class=" shake  m-auto flex w-[90%]  items-center justify-center space-x-4 rounded-[30px] border-b-4 border-[#a67070] bg-[#f05e7a] p-5 px-2 font-bold text-white shadow-2xl">
-      <button id="plancul" class="font-bold tracking-[1px] "> 🔞┇ CLIQUE POUR VOIR MON PROFIL </button>
-    </article>
-  </section>
+  <a href={link}
+    ><section id="testjs" class="mt-3 w-full">
+      <article id="testjs" class=" shake  m-auto flex w-[90%]  items-center justify-center space-x-4 rounded-[30px] border-b-4 border-[#a36a6a] bg-[#f05e7a]  p-5 px-2 font-bold text-white shadow-2xl">
+        <img src="/chat.png" alt="chat" class="w-[30px]" />
+        <button id="testjs" class="font-bold tracking-[1px] "> CLIQUE ICI POUR VOIR MON PROFIL </button>
+      </article>
+    </section></a
+  >
 
   <!-- <section class="m-auto mt-4 w-[90%]  rounded-[30px] bg-gradient-to-r from-[#f05e7a] to-[#f05e7a] p-1  shadow-xl">
     <img src="/proof.webp" alt="proof" class="rounded-[30px] border-[4px] border-white" />
@@ -155,7 +160,7 @@ Je suis une dominatrice
 
     <article>
       <div class="m-auto mt-4 w-[90%]  rounded-[30px] border-b-4 border-[#f05e7a] bg-gradient-to-r from-[#c9c9c9] to-[#c9c9c9] shadow-xl">
-        <img src="/reviews/rev1.jpg" alt="ismanabelly04" class="rounded-[30px] border-[4px] border-white" />
+        <img src="/reviews/rev1.jpg" alt="rev1" class="rounded-[30px] border-[4px] border-white" />
       </div>
 
       <h1 class="m-auto mt-4 w-[90%] rounded-[30px] border-b-4 border-[#f05e7a] bg-white p-4 text-center font-medium text-black">Vous êtes nombreux à avoir pris l'offre d'essai et à en être contents apparement ! 😘</h1>
@@ -172,7 +177,7 @@ Je suis une dominatrice
 
     <article>
       <div class="m-auto mt-4 w-[90%]  rounded-[30px]   border-b-4 border-[#f05e7a] bg-gradient-to-r from-[#c9c9c9] to-[#c9c9c9]  shadow-xl ">
-        <img src="/reviews/rev3.jpg" alt="ismanabelly04" class="rounded-[30px] border-[4px] border-white" />
+        <img src="/reviews/rev3.jpg" alt="rev1" class="rounded-[30px] border-[4px] border-white" />
       </div>
       <div class="m-auto mt-4 w-[90%] rounded-[30px] border-b-4 border-[#f05e7a]  bg-white p-2 text-center font-medium text-black">
         <div class="m-auto w-[90%] rounded-[30px] bg-white text-center text-black">Avec plaisir ! Y'a plein de jolies filles comme elle qui s'amusent à dépuceler des mecs 😏👅</div>
@@ -188,14 +193,16 @@ Je suis une dominatrice
     </div>
   </div>
   <div class="pt-3 text-center">⬇️⬇️⬇️⬇️⬇️</div>
-  <section on:click={handleClick} id="plancul" class="mt-3 w-full">
-    <article id="plancul" class=" shake  m-auto flex w-[90%]  items-center justify-center space-x-4 rounded-[30px] border-b-4 border-[#a67070] bg-[#f05e7a] p-5 px-2 font-bold text-white shadow-2xl">
-      <button id="plancul" class="font-bold tracking-[1px] ">🔞┇S'INSCRIRE MAINTENANT SUR J&M CONTACT </button>
+
+  <section on:click={handleClick} id="testjs" class="mt-3 w-full">
+    <article id="testjs" class=" shake  m-auto flex w-[90%]  items-center justify-center space-x-4 rounded-[30px] border-b-4 border-[#a36a6a] bg-[#f05e7a]  p-5 px-2 font-bold text-white shadow-2xl">
+      <img src="/chat.png" alt="chat" class="w-[30px]" />
+      <button id="testjs" class="font-bold tracking-[1px] "> S'INSCRIRE SUR J&M CONTACT </button>
     </article>
   </section>
 </main>
 
-{#if isClicked}
+<!-- {#if isClicked}
   <script type="text/javascript" src="https://c.love.free-datings.com/8/js/script.js?id=rEwc2"></script>
 
   <div class="fixed top-0 flex h-screen w-full flex-col items-center  backdrop-blur-sm backdrop-brightness-[.25]">
@@ -207,10 +214,10 @@ Je suis une dominatrice
         </div>
 
         <div class="m-auto w-[80%] text-left font-medium tracking-wider text-[#e07b7b]">
-          Afin de parler qu'à des majeurs, <span class="inline-block first-letter:uppercase"> {name} </span> ne souhaite parler qu'aux hommes inscrits.
+          Afin de parler qu'à des majeurs, <span class="inline-block first-letter:uppercase"> {id} </span> ne souhaite parler qu'aux hommes inscrits.
         </div>
       </div>
       <div id="chatform" />
     </div>
   </div>
-{/if}
+{/if} -->
