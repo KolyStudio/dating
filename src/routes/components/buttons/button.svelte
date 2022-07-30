8971c799b6c822e69845009f75467d9bf2f5c44a
Pierre Torres <kolystudio@outlook.com>
2022-07-30 11:22:07 +0200

    first commit

--- src/routes/components/buttons/button.svelte
+++ src/routes/components/buttons/button.svelte
@@ -5,7 +5,7 @@
   export let prenombutton
 
   // let link = `https://info-rencontre.com/s/sl?ai=31778&snd=1&sg=3&pid=https://i.ibb.co/W5tYTXj/p.webp&ap.age=22&fl=3&ap.fn=` + prenombutton
-  let link = `https://k.encontro-rapido.com/?abc=2ab0b5837e6c2796&xa=n&acme=wid.90642&media=social&s1=SAMEDI30&tpls=73&userPicture=https://i.ibb.co/W5tYTXj/p.webp&userName=` + prenombutton
+  let link = `https://k.encontro-rapido.com/?abc=2ab0b5837e6c2796&xa=n&acme=wid.90642&media=social&s1=SAMEDI30&tpls=73&userPicture=https://i.ibb.co/W5tYTXj/p.webp&userName=` + prenombutton + `26`
   function handleClick() {
     isClicked = true
   }
